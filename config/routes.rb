8cae75f3838d3d58a1ef7b3b4ed51623de2b6879
JCGuest <jcguest90@gmail.com>
2020-02-03 16:28:08 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  post '/' => 'products#add'
+#Create the routes for the application, 
+# we only need two routes, one to display the 
+# Products#index (the root route) and one to post 
+# the products to add them to the cart.
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
