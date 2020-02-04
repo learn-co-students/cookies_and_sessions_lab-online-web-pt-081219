75bb26d4f76d81ec2f19fcec38f2b07cfe6d6700
Shaqqour <mshaqqourm@gmail.com>
2020-02-03 19:47:24 -0800

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,9 @@
 class ProductsController < ApplicationController
 
     def index
-        @cart = cart || []
     end
 
     def add
-        cart = session[:cart] || []
         cart << params[:product]
         render :index
     end
